4f697867efd766e281861e480f67de7ad43a93a1
Amberhaart <bakroland0@gmail.com>
2018-10-29 14:05:04 +0100

    exercise 2 done

---
+++ week-02/day-01/AppendA/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create a string variable named `typo` and assign the value `Chinchill` to it
+    // - Write a function called `appendAFunc` that gets a string as an input,
+    //   appends an 'a' character to its end and returns with a string
+    //
+    // - Print the result of `appendAFunc(typo)`
+
+    return 0;
+}
