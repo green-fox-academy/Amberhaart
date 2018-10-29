d51cc6fdee0a85a3a353e78035d18a0a19c3bf18
Amberhaart <bakroland0@gmail.com>
2018-10-29 16:59:00 +0100

    chore(factorio)

--- week-02/day-01/Factorio/main.cpp
+++ week-02/day-01/Factorio/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <string>
+
+int factorio(int x);
+
+
+int main(int argc, char* args[]) {
+
+    // - Create a function called `factorio`
+    //   that returns it's input's factorial
+
+    int Roland = 5;
+
+    std::cout << factorio(Roland) << std::endl;
+
+
     return 0;
+}
+
+int factorio(int x) {
+
+    if (x > 1)
+    return x * factorio(x-1);
+
+    else return 1;
+
 }
