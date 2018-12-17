fd841da18eca3da680d45977975bd19973e3e547
Amberhaart <bakroland0@gmail.com>
2018-12-17 16:37:11 +0100

    added first four exercises

---
+++ week-09/Todos/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+  ];
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//app.use(express.json());
+
+app.get('/',(req, res) => {
+    // render `home.ejs`
+    res.render('home', {todos: todos});
+  });
+
+// start express app on port 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
