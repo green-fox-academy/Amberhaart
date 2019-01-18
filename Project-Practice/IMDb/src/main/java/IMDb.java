87b785da88a98fb92ebd5e7cb31f05344c3fb0f2
Amberhaart <bakroland0@gmail.com>
2019-01-18 10:06:31 +0100

    feat(IMDb.java): added wait function until searchbar properly loads

--- Project-Practice/IMDb/src/main/java/IMDb.java
+++ Project-Practice/IMDb/src/main/java/IMDb.java
@@ -2,12 +2,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class IMDb {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "/Users/Roland/Gecko-drivers/Mozilla/geckodriver");
         WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
 
         driver.get("http://imdb.com");
         driver.findElement(By.id("imdb-signin-link")).click();
@@ -15,9 +19,10 @@ public class IMDb {
         driver.findElement(By.id("ap_email")).sendKeys("bakroland1@gmail.com");
         driver.findElement(By.id("ap_password")).sendKeys("pyHZU5frSZTUsaf");
         driver.findElement(By.id("signInSubmit")).click();
-        driver.findElement(By.id("navbar-query")).sendKeys("Guardians of the Galaxy 2"); //sometimes works, sometimes it doesn't
+        WebElement searchbar = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("navbar-query")));
+        searchbar.sendKeys("Guardians of the Galaxy 2");
+       // driver.findElement(By.id("navbar-query")).sendKeys("Guardians of the Galaxy 2"); //sometimes works, sometimes it doesn't
         driver.findElement(By.id("navbar-submit-button")).click();
-       // driver.findElement(By.
 
 //        driver.quit();
     }
