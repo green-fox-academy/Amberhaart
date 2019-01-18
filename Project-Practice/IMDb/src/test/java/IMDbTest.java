3582090fa9603ee39ca846ca722b81a7e11a1e85
Amberhaart <bakroland0@gmail.com>
2019-01-18 10:59:28 +0100

    feat(JavaTest.java): added test to open IMDb main page

--- Project-Practice/IMDb/src/test/java/IMDbTest.java
+++ Project-Practice/IMDb/src/test/java/IMDbTest.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class IMDbTest {
+
+    @Test
+    public void startWebDriver() {
+        System.setProperty("webdriver.gecko.driver", "/Users/Roland/Gecko-drivers/Mozilla/geckodriver");
+        WebDriver driverTest = new FirefoxDriver();
+
+        driverTest.navigate().to("http://www.imdb.com");
+
+        Assert.assertTrue("correct webpage loads", driverTest.getCurrentUrl().endsWith(".com"));
+    }
+
 }
